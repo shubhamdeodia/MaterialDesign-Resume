38e664d5475a866110f8e478d832599b0e7087e2
Shubham Deodia <deodia@de2mcl-000151.local>
2020-04-05 16:17:08 +0200

    Updates

---
+++ precache-manifest.d81e5ddac956f463079c0e1d92328ed8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab11dae7e60964870bb0a91e41c87018",
+    "url": "/MaterialDesign-Resume/index.html"
+  },
+  {
+    "revision": "136bf51c2831b803cd30",
+    "url": "/MaterialDesign-Resume/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0ec46264a47df8f38882",
+    "url": "/MaterialDesign-Resume/static/js/2.6c3e8b07.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/MaterialDesign-Resume/static/js/2.6c3e8b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "136bf51c2831b803cd30",
+    "url": "/MaterialDesign-Resume/static/js/main.8f9e1801.chunk.js"
+  },
+  {
+    "revision": "4cd4ff16b5c338bed0be",
+    "url": "/MaterialDesign-Resume/static/js/runtime-main.618fb955.js"
+  },
+  {
+    "revision": "6afd24cf53cd4c892d5d435080feb5a0",
+    "url": "/MaterialDesign-Resume/static/media/Experience.6afd24cf.png"
+  },
+  {
+    "revision": "abe11cd6b7aa77944eb9234241a1a031",
+    "url": "/MaterialDesign-Resume/static/media/Skills.abe11cd6.png"
+  },
+  {
+    "revision": "cfa0a33bff0e8249ec3e2902a33ad8b7",
+    "url": "/MaterialDesign-Resume/static/media/avatar.cfa0a33b.jpg"
+  }
+]);
